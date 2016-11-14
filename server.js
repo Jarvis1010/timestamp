6a959b29c69b93677d4f3609f7f01e2c2fb6f019
Justin Travis Mair <travis.mair@gmail.com>
2016-11-14 21:54:59 +0000

    update to root path

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ Date.prototype.valid = function() {
 var port = process.env.PORT||8080;
 
 app.get('/', function(req,res){
-    res.send({});
+    res.send("Testing");
 });
 
 app.get('/:Date',function(req,res){
